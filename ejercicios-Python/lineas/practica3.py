5b0f5dba00f9ccb854075bd0d89d49c368802efb
andresVanegas56 <166045730+andresVanegas56@users.noreply.github.com>
2024-05-11 12:47:38 -0500

    segundo comentario

---
+++ ejercicios-Python/lineas/practica3.py
@@ -0,0 +1,9 @@
+#uso de la función f-string
+nombre="andres"
+profesion="Tecnologo de mantenimiento de computo"
+estatura=1.68
+peso=50
+print(f"mi nombre es:{nombre},soy: {profesion},mido: {estatura} cm, y peso: {peso} kg")
+pi=3.1416
+print("El valor de pi es: %.2f "% pi)
+print(f"El valor de pi es {pi: .4f}")
